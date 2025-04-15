4068f49b5108a97b160e563786b7ce1c0ffb2843
luis-henrique-santarem <lushenriquesantarem@gmail.com>
2025-04-15 17:26:50 -0300

    Add files via upload

---
+++ loginHash/src/main/java/com/mycompany/loginhash/model/Usuario.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.loginhash.model;
+
+/**
+ *
+ * @author LUISHENRIQUEPEDROSOS
+ */
+public class Usuario {
+    
+    private int id;
+    private String email;
+    private String senha;
+
+    public Usuario(String email, String senha) {
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+}
